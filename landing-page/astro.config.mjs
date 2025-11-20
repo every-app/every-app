9cc789783e3a3ee99ce50ad81f478e34f486bbf4
Ben Senescu <44480372+bensenescu@users.noreply.github.com>
2025-11-20 16:46:14 -0500

    r Improve Landing Page, Docs and OpenGraph Previews

--- landing-page/astro.config.mjs
+++ landing-page/astro.config.mjs
@@ -22,9 +22,52 @@ export default defineConfig({
       },
       customCss: ["./src/styles/custom.css"],
       components: {
-        Head: "./src/components/Head.astro",
         Header: "./src/components/Header.astro",
       },
+      head: [
+        // Custom favicons
+        {
+          tag: "link",
+          attrs: {
+            rel: "icon",
+            type: "image/x-icon",
+            href: "/favicon.ico",
+          },
+        },
+        {
+          tag: "link",
+          attrs: {
+            rel: "apple-touch-icon",
+            sizes: "180x180",
+            href: "/apple-touch-icon.png",
+          },
+        },
+        {
+          tag: "link",
+          attrs: {
+            rel: "icon",
+            type: "image/png",
+            sizes: "32x32",
+            href: "/favicon-32x32.png",
+          },
+        },
+        {
+          tag: "link",
+          attrs: {
+            rel: "icon",
+            type: "image/png",
+            sizes: "16x16",
+            href: "/favicon-16x16.png",
+          },
+        },
+        {
+          tag: "link",
+          attrs: {
+            rel: "manifest",
+            href: "/manifest.json",
+          },
+        },
+      ],
       social: {
         github: "https://github.com/every-app/every-app",
         discord: "https://discord.gg/EpwPGyDn",
