0bc98764471018a7f5c86a1b1b799d4fa12f720d
Ben Senescu <44480372+bensenescu@users.noreply.github.com>
2025-12-20 13:55:05 -0500

    Improve todo list item hover (#41)

--- apps/todo-app/src/client/components/TodoHistoryItem.tsx
+++ apps/todo-app/src/client/components/TodoHistoryItem.tsx
@@ -14,7 +14,7 @@ export function HistoryItem({ todo }: HistoryItemProps) {
 
   return (
     <div
-      className={`flex items-center gap-3 p-2 rounded-md hover:bg-gray-100 transition-colors ${!isMobile ? "group" : ""}`}
+      className={`flex items-center gap-3 p-2 rounded-md border border-transparent hover:border-base-300 hover:bg-base-300/50 transition-all ${!isMobile ? "group" : ""}`}
     >
       <Checkbox
         checked={todo.completed}
