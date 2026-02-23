b33a6551a8da38c6adc863e3054e0952f127d591
Ben Senescu <44480372+bensenescu@users.noreply.github.com>
2026-02-23 14:08:12 -0500

    Add VITE_ prefix to BYPASS_GATEWAY env var (#178)

--- templates/simple-todo/vite.config.ts
+++ templates/simple-todo/vite.config.ts
@@ -6,6 +6,8 @@ import tailwindcss from "@tailwindcss/vite";
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
+  // If there is the env var, create another env var with the VITE_ prefix for the frontend
+  envPrefix: ["VITE_", "BYPASS_GATEWAY_LOCAL_ONLY"],
   server: {
     port: 3001,
   },
